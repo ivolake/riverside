28c06b4b9b615a7555740244e3a551fa4bb0668c
ivolake <42046928+ivolake@users.noreply.github.com>
2019-01-05 22:34:33 +0300

    for netpage

---
+++ Networks_Science/main_for_andrey.py
@@ -0,0 +1,31 @@
+from calc import main
+# cd Documents\Python_Scripts\Networks_Science
+
+
+if args[1] == '0':
+    main(args[1], args[2], args[3], args[4], args[5])
+
+elif args[1] == '1':
+    main(args[1], args[2], args[3], args[4], args[5], args[6], args[7])
+
+elif args[1] == '2':
+    main(args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9])
+
+elif args[1] == '3':
+    main(args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9])
+
+elif args[1] == '4':
+    main(args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10])
+
+'''
+TODO list
+
+1) Сделать ввод запрещенных узлов через info файл
+...
+100500) Передача множества пакетов в правильном порядке (???)
+
+Цель: Приложение, РЕАЛЬНОЕ ВРЕМЯ
+1) Создать графический интерфейс, Tkinter
+
+
+'''
